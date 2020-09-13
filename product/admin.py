c66da9e2c402a4935989fb59d6be1024378aba96
jetigenov <jetigenov@gmail.com>
2020-09-14 00:28:52 +0600

    fixed all

--- product/admin.py
+++ product/admin.py
@@ -4,7 +4,6 @@ from mptt.admin import DraggableMPTTAdmin
 from product.models import Category, Product, Images, Comment
 
 
-
 class CategoryAdmin(DraggableMPTTAdmin):
     mptt_indent_field = "title"
     list_display = ('tree_actions', 'indented_title',
@@ -17,26 +16,28 @@ class CategoryAdmin(DraggableMPTTAdmin):
 
         # Add cumulative product count
         qs = Category.objects.add_related_count(
-                qs,
-                Product,
-                'category',
-                'products_cumulative_count',
-                cumulative=True)
+            qs,
+            Product,
+            'category',
+            'products_cumulative_count',
+            cumulative=True)
 
         # Add non cumulative product count
         qs = Category.objects.add_related_count(qs,
-                 Product,
-                 'category',
-                 'products_count',
-                 cumulative=False)
+                                                Product,
+                                                'category',
+                                                'products_count',
+                                                cumulative=False)
         return qs
 
     def related_products_count(self, instance):
         return instance.products_count
+
     related_products_count.short_description = 'Related products (for this specific category)'
 
     def related_products_cumulative_count(self, instance):
         return instance.products_cumulative_count
+
     related_products_cumulative_count.short_description = 'Related products (in tree)'
 
 
@@ -52,10 +53,12 @@ class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImageInline]
     prepopulated_fields = {'slug': ('title',)}
 
+
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['subject', 'comment', 'status', 'created']
     list_filter = ['status']
     readonly_fields = ['subject', 'comment', 'ip', 'user', 'product', 'rate', ]
+
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 from product.models import Category
+from user.forms import SignUpForm
 from user.models import UserProfile
 
 
@@ -12,6 +13,7 @@ def index(request):
 
 
 def login_form(request):
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -35,10 +37,32 @@ def login_form(request):
 
 
 def signup_form(request):
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            # Create data in profile table for user
+            current_user = request.user
+            data = UserProfile()
+            data.user_id = current_user.id
+            data.image = "images/users/user.png"
+            data.save()
+            messages.success(request, 'Your account has been created!')
+            return HttpResponseRedirect('/')
+        else:
+            messages.warning(request, form.errors)
+            return HttpResponseRedirect('/signup')
+
+    form = SignUpForm()
     category = Category.objects.all()
-    context = {
-        'category': category
-    }
+    context = {'form': form,
+               'category': category,
+               }
     return render(request, 'signup_form.html', context)
 
 
